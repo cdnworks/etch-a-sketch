4c20b2e5f049cb0a9e4f72798ee5f6175b60694a
Chris <chrisdnielson@gmail.com>
2022-10-02 12:57:27 -0700

    Add a resolution slider and minor refactoring
    Added a simple slider that changes the 'resolution' of the canvas,
    wherein the number of squares is goverened by the value of the slider.
    The slider works to make a square canvas, in increments of 8 squares
    per side, from 8 squares to 128 squares.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
-function renderCanvas() {
+function renderCanvas(h, w) {
     //create and place a grid of square divs
     //these divs will be placed inside a container div, class="canvas"
     
     //expansion idea: take some parameters that define the size of the canvas
     //where size is the resolution or number of squares on the canvas
 
-    let height = 32;
-    let width = 32;
+    let height = h;
+    let width = w;
 
     const canvas = document.querySelector('.canvas');
+
+    //clear canvas area
+    canvas.textContent = '';
 
     //create a row, then in said row, appends squares, then append row
     for(let i = 1; i <= height; i ++) {
@@ -23,35 +26,53 @@ function renderCanvas() {
         canvas.appendChild(row);
     }
 
+    let canvasSquares = document.querySelectorAll('.square');
+    drawToCanvas(canvasSquares);
+
 }
 
-renderCanvas();
 
-//testing vars
-let color = 'black';
+function drawToCanvas(canvasSquares) {
+    let color = 'black';
+    //mouse events
+    canvasSquares.forEach((square) => {
+        square.addEventListener('mouseover', function(event) {
+            //color preview
+            let originalColor = square.style.backgroundColor; //get the original color
+            square.style.backgroundColor = color; //color the square
 
-let canvasSquares = document.querySelectorAll('.square');
+            //listen for mouse out
+            square.addEventListener('mouseout', function(e) {
+                //revert to the original color
+                square.style.backgroundColor = originalColor;
+            })
 
-//events
-canvasSquares.forEach((square) => {
-    square.addEventListener('mouseover', function(event) {
-        //color preview
-        let originalColor = square.style.backgroundColor; //get the original color
-        square.style.backgroundColor = color; //color the square
+            square.addEventListener('mousedown', function(event) {
+                //commit new color to the square, and 'originalColor'
+                originalColor = color;
+                square.style.backgroundColor = color;
+            });
 
-        //listen for mouse out
-        square.addEventListener('mouseout', function(e) {
-            //revert to the original color
-            square.style.backgroundColor = originalColor;
-        })
 
-        square.addEventListener('mousedown', function(event) {
-            //commit new color to the square, and 'originalColor'
-            originalColor = color;
-            square.style.backgroundColor = color;
         });
 
-
     });
+}
 
-});
+
+renderCanvas(8, 8);
+
+//event vars
+
+
+
+let slider = document.getElementById("resolution-slider");
+let sliderInfo = document.getElementById("slider-info");
+sliderInfo.innerText = `${slider.value} x ${slider.value}`;
+
+//slider events
+slider.oninput = function() {
+    sliderInfo.innerText = `${this.value} x ${this.value}`;
+    let dimension = parseInt(this.value);
+    renderCanvas(dimension, dimension);
+}
